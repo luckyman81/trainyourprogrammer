c45adae84549b32e7c0a92e18aa8bbf54d776162
Stefan Obergröbner <obergroebner@kabelmail.de>
2020-04-20 23:45:27 +0200

    Added new projects

---
+++ CS_Aufgabe_239_LiouvilleSerie/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Shapes;
+
+namespace CS_Aufgabe_239_LiouvilleSerie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Width = 700;
+            Height = 700;
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            int LiouX = 0;
+            int LiouY = 0;
+
+            int startX = 100;
+            int startY = 100;
+            List<Point> points = new List<Point>();
+
+            using (StreamWriter stream = new StreamWriter(@"C:\Users\Stefan\Desktop\liouville.txt"))
+            {
+                for (int n = 1; n < 10000; n++)
+                {
+                    short liou = Liouville(n, stream);
+                    if (n % 2 == 0)
+                    {
+                        LiouY = LiouY + liou;
+                    }
+                    else
+                    {
+                        LiouX = LiouX + liou;
+                    }
+                    points.Add(new Point(startX-LiouX,startY+ LiouY));
+                }
+            }
+
+
+            g.DrawCurve(Pens.Red, points.ToArray());
+
+
+        }
+
+
+        private static short Liouville(int n, StreamWriter stream)
+        {
+            if (n == 1)
+            {
+                stream.WriteLine(1);
+                return 1;
+            }
+
+            List<int> primes = SieveEratosthenes(n + 1);
+
+            List<int> primefactors = PrimeDecomp(n, primes);
+
+            if (primefactors.Count % 2 == 0)
+            {
+                stream.WriteLine(1);
+                return 1;
+            }
+            else
+            {
+                stream.WriteLine(-1);
+                return -1;
+            }
+
+        }
+
+        private static List<int> PrimeDecomp(int m, List<int> primes)
+        {
+            List<int> factors = new List<int>();
+
+            foreach (int i in primes)
+            {
+                while (m % i == 0)
+                {
+                    factors.Add(i);
+                    m /= i;
+                }
+            }
+
+            return factors;
+        }
+
+        private static List<int> SieveEratosthenes(int n)
+        {
+            List<int> primes = new List<int>();
+            bool[] prime = new bool[n];
+            //Zuerst alle Zahlen von 2 bis n als Primzahl markieren
+            for (int i = 2; i < n; i++)
+            {
+                prime[i] = true;
+            }
+
+            //Einzelner Abschnitt
+            {
+                //Wir wollen bei 2 anfangen
+                int i = 2;
+
+                //Alle Produkte des Teilers i
+                //angefangen bei 2, bis kleiner n durchlaufen
+                //Wenn n = 50, dann ist bei i = 7 Schluss, weil das Produkt = 49 ist
+                for (; i * i < n; i++)
+                {
+                    //Wenn die Zahl im Array als Primzahl markiert ist
+                    //Was bei den ersten beiden 2 und 3 definitiv der Fall ist
+                    if (prime[i])
+                    {
+                        //Primzahl bis Wurzel(n) ausgeben
+                        Console.WriteLine(i);
+                        primes.Add(i);
+                        //Alle weiteren Produkte des Teilers i
+                        //angefangen beim Produkt i * i bis kleiner n durchlaufen
+                        //j wird mit i beim nächsten Durchlauf (Vielfaches) addiert
+                        for (int j = i * i; j < n; j += i)
+                        {
+                            //Dies kann unmöglich eine Primzahl sein
+                            //weil es ein Vielfaches von i ist.
+                            prime[j] = false;
+                        }
+                    }
+                }
+
+                //Alle Primzahlen ausgebene
+                for (; i < n; i++)
+                {
+                    if (prime[i])
+                    {
+                        Console.WriteLine(i);
+                        primes.Add(i);
+                    }
+                }
+            }
+            return primes;
+        }
+
+
+    }
+}
+
+
---
+++ CS_Aufgabe_90_Kreiszahl/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CS_Aufgabe_90_Kreiszahl
+{
+    /*
+     *  Man kann die Kreiszahl Pi näherungsweise bestimmen, indem man Zufallszahlen generiert:
+        Man stelle sich einen Kreis mit Mittelpunkt (0/0) und Radius 1 vor. Es werden zufällig Punkte erzeugt, bei denen sowohl x als auch y im Intervall [0;1[ liegen. Dann wird die Entfernung dieser Punkte zum Ursprung ermittelt. Ist diese Entfernung kleiner als 1, so liegt der Punkt innerhalb des Kreises.
+        Setzt man bei einer ausreichenden Zahl von Zufallspunkten die Zahl der Treffer in das richtigen Verhältnis zur Gesamtzahl der Punkte, so erhält man einen Näherungswert für Pi (Pi = 4 * AnzahlTreffer / AnzahlGesamt).
+
+        Schreibe ein Programm, das auf oben beschriebene Weise Pi berechnet.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int AnzahlTreffer = 0; 
+            int AnzahlGesamt = 0;
+            Random rnd = new Random();
+
+            for (int i = 0; i < 1e5; i++)
+            {
+                double dist = Math.Sqrt(Math.Pow(rnd.NextDouble(), 2.0) + Math.Pow(rnd.NextDouble(), 2.0));
+                if (dist < 1e0) { AnzahlTreffer++; }
+                AnzahlGesamt++;
+            }
+
+            double Pi = 4.0 * AnzahlTreffer / AnzahlGesamt;
+
+            Console.WriteLine($"Abweichung nach {AnzahlGesamt} Versuchen: {Pi-Math.PI}");
+
+            Console.ReadKey();
+        }
+    }
+}
